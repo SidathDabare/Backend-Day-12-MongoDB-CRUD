4e7ecc0cd6128b1c84cdb58d4aebd5db36fb2ebc
SidathDabare <sidath2007@gmail.com>
2022-08-02 15:35:34 +0200

    Initial commit

---
+++ Backend-Day-12-MongoDB-CRUD-Backend/src/api/users/index.js
@@ -0,0 +1,85 @@
+/** @format */
+
+import express from "express"
+import createHttpError from "http-errors"
+import BlogPostsModel from "./model.js"
+
+const blogPostsRouter = express.Router()
+
+blogPostsRouter.post("/", async (req, res, next) => {
+  try {
+    const newUser = new BlogPostsModel(req.body) // here it happens the validation (thanks to Mongoose) of request body, if it is not ok Mongoose will throw an error (if it is ok it is NOT saved yet)
+    const { _id } = await newUser.save()
+
+    res.status(201).send({ _id })
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogPostsRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await BlogPostsModel.find()
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogPostsRouter.get("/:postId", async (req, res, next) => {
+  try {
+    const post = await BlogPostsModel.findById(req.params.postId)
+    if (user) {
+      res.send(user)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.postId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogPostsRouter.put("/:postId", async (req, res, next) => {
+  try {
+    const updatedUser = await BlogPostsModel.findByIdAndUpdate(
+      req.params.postId, // WHO you want to modify
+      req.body, // HOW you want to modify
+      { new: true, runValidators: true } // OPTIONS. By default findByIdAndUpdate returns the record pre-modification. If you want to get back the newly update record you should use the option new: true
+      // By default validation is off here --> runValidators: true
+    )
+    // ************************************************* ALTERNATIVE METHOD *******************************************************
+
+    // const user = await BlogPostsModel.findById(req.params.userId) // when you do a findById, findOne, etc,... you get back a MONGOOSE DOCUMENT which is NOT a normal object but an object with some superpowers (like the .save() method) that will be useful in the future
+
+    // user.firstName = "Diego"
+
+    // await user.save()
+
+    // res.send(user)
+
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogPostsRouter.delete("/:userId", async (req, res, next) => {
+  try {
+    const deletedUser = await BlogPostsModel.findByIdAndDelete(
+      req.params.userId
+    )
+    if (deletedUser) {
+      res.status(204).send()
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default blogPostsRouter
---
+++ Backend-Day-12-MongoDB-CRUD-Backend/src/api/users/model.js
@@ -0,0 +1,28 @@
+/** @format */
+
+import mongoose from "mongoose"
+
+const { Schema, model } = mongoose
+
+const blogPostsSchema = new Schema(
+  {
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    cover: { type: String, required: true },
+    professions: [String],
+    readTime: {
+      value: { type: Number },
+      unit: { type: String },
+    },
+    author: {
+      name: { type: String },
+      avatar: { type: String },
+    },
+    content: { type: String, required: false },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default model("blogPosts", blogPostsSchema)
